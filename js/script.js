59a1ef4228388b46381ba5eff1ca9196fc5931f0
anniki19 <nastya-nikitin@mail.ru>
2019-08-19 17:18:50 +0300

    new block

--- js/script.js
+++ js/script.js
@@ -1,50 +1,159 @@
 $(document).ready(function() {
+    $('.demo').slick({
+        // Enables tabbing and arrow key navigation
+        accessibility: true,
+        // Adapts slider height to the current slide
+        adaptiveHeight: false,
 
-    $("#mydiv")
-        .animate({ height: "hide" }, 300)
-        .text("Новый текст")
-        .animate({ height: "show" }, 300);
-});
+        // Change where the navigation arrows are attached (Selector, htmlString, Array, Element, jQuery object)
 
-$('.jcarousel')
-    .on('jcarousel:create jcarousel:reload', function() {
-        var element = $(this),
-            width = element.innerWidth();
+        //appendArrows: $(element),
 
-        if (width > 900) {
-            width = width / 3;
-        } else if (width > 600) {
-            width = width / 2;
-        }
+        // Change where the navigation dots are attached (Selector, htmlString, Array, Element, jQuery object)
 
-        element.jcarousel('items').css('width', width + 'px');
-    })
-    .jcarousel({
-        // Your configurations options
+        //appendDots: $(element),
+
+        // Enable Next/Prev arrows
+        arrows: true,
+
+        // Sets the slider to be the navigation of other slider (Class or ID Name)
+
+        asNavFor: null,
+
+        // prev arrow
+
+        prevArrow: '<button type="button" data-role="none" class="slick-prev">Previous</button>',
+
+        // next arrow
+
+        nextArrow: '<button type="button" data-role="none" class="slick-next">Next</button>',
+
+        // Enables auto play of slides
+        autoplay: true,
+
+        // Auto play change interval
+        autoplaySpeed: 3000,
+
+        // Enables centered view with partial prev/next slides.
+        // Use with odd numbered slidesToShow counts.
+        centerMode: true,
+
+        // Side padding when in center mode. (px or %)
+        centerPadding: '50px',
+
+        // CSS3 easing
+        cssEase: 'ease',
+
+        // Custom paging templates.
+        customPaging: function(slider, i) {
+            return '<button type="button" data-role="none">' + (i + 1) + '</button>';
+        },
+
+        // Current slide indicator dots
+        dots: false,
+
+        // Class for slide indicator dots container
+        dotsClass: 'slick-dots',
+
+        // Enables desktop dragging
+        draggable: true,
+
+        // animate() fallback easing
+        easing: 'linear',
+
+        // Resistance when swiping edges of non-infinite carousels
+
+        edgeFriction: 0.35,
+
+        // Enables fade
+        fade: false,
+
+        // Focus on select and/or change
+        focusOnSelect: false,
+
+        focusOnChange: false,
+
+        // Infinite looping
+        infinite: true,
+
+        // Initial slide
+        initialSlide: 0,
+
+        // Accepts 'ondemand' or 'progressive' for lazy load technique
+        lazyLoad: 'ondemand',
+
+        // Mobile first
+        mobileFirst: false,
+
+        // Pauses autoplay on hover
+        pauseOnHover: true,
+
+        // Pauses autoplay on focus
+        pauseOnFocus: true,
+
+        // Pauses autoplay when a dot is hovered
+        pauseOnDotsHover: false,
+
+        // Target containet to respond to
+        respondTo: 'window',
+
+        // Breakpoint triggered settings
+        responsive: null,
+
+        // Setting this to more than 1 initializes <a href="https://www.jqueryscript.net/tags.php?/grid/">grid</a> mode.
+        // Use slidesPerRow to set how many slides should be in each row.
+        rows: 1,
+
+        // Change the slider's direction to become right-to-left
+        rtl: false,
+
+        // Slide element query
+        slide: '',
+
+        // # of slides to show at a time
+        slidesToShow: 1,
+
+        // With grid mode intialized via the rows option, this sets how many slides are in each grid row.
+        slidesPerRow: 1,
+
+        // # of slides to scroll at a time
+        //slidesTo <a href = "https://www.jqueryscript.net/tags.php?/Scroll/"> Scroll < /a> : 1,
+
+        // Transition speed
+        speed: 300,
+
+        // Enables touch swipe
+        swipe: true,
+
+        // Swipe to slide irrespective of slidesToScroll
+        swipeToSlide: false,
+
+        // Enables slide moving with touch
+        touchMove: true,
+
+        // To advance slides, the user must swipe a length of (1/touchThreshold) * the width of the slider.
+        touchThreshold: 5,
+
+        // Enable/Disable CSS Transitions
+        useCSS: true,
+
+        // Enable/Disable CSS Transforms
+        useTransform: true,
+
+        // Disables automatic slide width calculation
+        variableWidth: false,
+
+        // Vertical slide direction
+        vertical: false,
+
+        // hanges swipe direction to vertical
+        verticalSwiping: false,
+
+        // Ignores requests to advance the slide while animating
+        waitForAnimate: true,
+
+        // Set the zIndex values for slides, useful for IE9 and lower
+        zIndex: 1000
+
     });
-
-// $(document).ready(function() {
-//     var leftUIEl = $('.slider-arrow-left');
-//     var rightUIEl = $('.slider-arrow-right');
-//     var elementsList = $('.slider-list');
-
-//     var pixelsOffset = 125;
-//     var currentLeftValue = 0;
-//     var elementsCount = elementsList.find('li').length;
-//     var minimumOffset = -((elementsCount - 5) * pixelsOffset);
-//     var maximumOffset = 0;
-
-//     leftUIEl.click(function() {
-//         if (currentLeftValue != maximumOffset) {
-//             currentLeftValue += 125;
-//             elementsList.animate({ left: currentLeftValue + "px" }, 500);
-//         }
-//     });
-
-//     rightUIEl.click(function() {
-//         if (currentLeftValue != minimumOffset) {
-//             currentLeftValue -= 125;
-//             elementsList.animate({ left: currentLeftValue + "px" }, 500);
-//         }
-//     });
-// });
+});
