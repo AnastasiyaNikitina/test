35b54c07f927d858fc0e89fada7baa7786dab2b7
anniki19 <nastya-nikitin@mail.ru>
2019-08-20 18:43:00 +0300

    nav

--- js/script.js
+++ js/script.js
@@ -24,3 +24,47 @@ $(document).ready(function() {
         autoplaySpeed: 2000,
     });
 });
+
+
+$(".main-header li ul").hide(); // скрываем выпадающее меню
+$(".main-header li table").hide();
+
+$(".main-header li:has('.submenu-1')").hover(
+    function() {
+        $(".submenu-1").stop().fadeToggle(200);
+    }
+    // отбираем элемент списка, который содержит элемент с классом 
+    //.submenu и добавляем ему функцию при наведении, которая 
+    //показывает и скрывает выпадающее меню */
+);
+
+$(".main-header li:has('.submenu-2')").hover(
+    function() {
+        $(".submenu-2").stop().fadeToggle(200);
+    }
+);
+
+$(".main-header li:has('.submenu-3')").hover(
+    function() {
+        $(".submenu-3").stop().fadeToggle(200);
+    }
+);
+
+$(".main-header li:has('.submenu-4')").hover(
+    function() {
+        $(".submenu-4").stop().fadeToggle(200);
+    }
+);
+
+$(".main-header li:has('.submenu-5')").hover(
+    function() {
+        $(".submenu-5").stop().fadeToggle(200);
+
+    }
+);
+
+$(".main-header li:has('.submenu-6')").hover(
+    function() {
+        $(".submenu-6").stop().fadeToggle(200);
+    }
+);
